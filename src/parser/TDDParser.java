14204122984873f7c95ff43d2e005de61f5603a0
Victor Hugo Dias Coelho <victorhdcoelho@gmail.com>
2020-11-23 12:12:02 -0300

    Add falsification method to parser

---
+++ src/parser/TDDParser.java
@@ -0,0 +1,18 @@
+package parser;
+
+public class TDDParser {
+	private char separator;
+	private String target_file;
+	
+//	private TDDParser(char separator, String target_file)
+//	{
+//		this.separator = separator;
+//		this.target_file = target_file;
+//		
+//	}
+	
+	public char getSeparator()
+	{
+		return ';'; // Falsificação
+	}
+}
---
+++ src/tst/TstParser.java
@@ -0,0 +1,17 @@
+package tst;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import parser.TDDParser;
+
+class TstParser {
+	@Test
+	void testParser01() {
+		TDDParser parser = new TDDParser();
+		assertEquals(';', parser.getSeparator());
+	}
+
+}
